2553fabb5074c3853dd885d3da05d54843102a88
k-sante250 <yaojiantong@ufl.edu>
2025-11-04 16:36:21 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -2,6 +2,7 @@
 
 void Graph::addEdge(int from, int to, double weight) {
     adjList[from].push_back({to, weight});
+    adjList[to].push_back({from, weight});
 }
 
 const std::unordered_map<int, std::vector<std::pair<int, double>>>& Graph::getAdjList() const {
